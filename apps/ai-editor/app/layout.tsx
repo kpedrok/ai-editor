bd40faecf55055b78f8436085e90d73d601ca57a
Pedro Boas <pedrokvb@gmail.com>
2024-06-05 19:17:10 -0300

    update metadata

--- apps/ai-editor/app/layout.tsx
+++ apps/ai-editor/app/layout.tsx
@@ -5,25 +5,28 @@ import type { Metadata, Viewport } from "next";
 import type { ReactNode } from "react";
 import Providers from "./providers";
 
-const title = "Novel - Notion-style WYSIWYG editor with AI-powered autocompletions";
-const description =
-  "Novel is a Notion-style WYSIWYG editor with AI-powered autocompletions. Built with Tiptap, OpenAI, and Vercel AI SDK.";
-
 export const metadata: Metadata = {
-  title,
-  description,
-  openGraph: {
-    title,
-    description,
+  title: {
+    default: 'RewritePal | Write Better, Communicate Better, Deliver More',
+    template: '%s | RewritePal',
   },
-  twitter: {
-    title,
-    description,
-    card: "summary_large_image",
-    creator: "@steventey",
-  },
-  metadataBase: new URL("https://novel.sh"),
-};
+  description: `Your Free AI Writing Tool. Paraphrasing tool, improve any paragraph's readability and rewrite it to make it sound more human-like with this powerful free tool.`,
+  keywords: [
+    'paraphrasing',
+    'rewrite',
+    'grammar checker',
+    'AI writing assistant',
+    'proofreading tool',
+    'writing tool',
+    'grammar tool',
+    'free writing assistant',
+    'language improvement',
+    'online editor',
+    'spelling check',
+    'punctuation checker',
+  ],
+  robots: 'index, follow',
+}
 
 export const viewport: Viewport = {
   themeColor: "#ffffff",
