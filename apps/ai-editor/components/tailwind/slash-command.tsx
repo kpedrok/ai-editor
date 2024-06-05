6387ad705016f2cbb580b4fbd8ac9e7e473402d8
Pedro Boas <pedrokvb@gmail.com>
2024-06-05 18:47:18 -0300

    update folder structure

--- apps/ai-editor/components/tailwind/slash-command.tsx
+++ apps/ai-editor/components/tailwind/slash-command.tsx
@@ -7,23 +7,24 @@ import {
   ImageIcon,
   List,
   ListOrdered,
+  MessageSquarePlus,
   Text,
   TextQuote,
-  Youtube
+  Youtube,
 } from "lucide-react";
 import { Command, createSuggestionItems, renderItems } from "novel/extensions";
 import { uploadFn } from "./image-upload";
 
 export const suggestionItems = createSuggestionItems([
-  // {
-  //   title: "Send Feedback",
-  //   description: "Let us know how we can improve.",
-  //   icon: <MessageSquarePlus size={18} />,
-  //   command: ({ editor, range }) => {
-  //     editor.chain().focus().deleteRange(range).run();
-  //     window.open("/feedback", "_blank");
-  //   },
-  // },
+  {
+    title: "Send Feedback",
+    description: "Let us know how we can improve.",
+    icon: <MessageSquarePlus size={18} />,
+    command: ({ editor, range }) => {
+      editor.chain().focus().deleteRange(range).run();
+      window.open("/feedback", "_blank");
+    },
+  },
   {
     title: "Text",
     description: "Just start typing with plain text.",
